604151b169fec7f607d827b1be27722efdf4bbf4
inves13 <investh13@gmail.com>
2025-10-23 08:46:41 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,19 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('hinvest-cache').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json'
+      ]);
+    })
+  );
+});
 
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
